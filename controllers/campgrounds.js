182f3461d21fd683508306dd5191e9defe48ac97
virlandanut <virlan.danut@outlook.com>
2024-02-10 16:07:40 +0200

    Patch 1.8.1
    Restructurare cod, adăugare controllers și rezolvare bug-uri minore!

---
+++ controllers/campgrounds.js
@@ -0,0 +1,60 @@
+const Campground = require("../models/campground");
+
+module.exports.index = async (request, response) => {
+  const campgrounds = await Campground.find({});
+  response.render("campgrounds/index", { campgrounds });
+};
+
+module.exports.renderNewForm = async (request, response) => {
+  response.render("campgrounds/new");
+};
+
+module.exports.createCampground = async (request, response, next) => {
+  const { campground } = request.body;
+  const camp = new Campground(campground);
+  camp.author = request.user._id;
+  await camp.save();
+  request.flash("success", "Locul de campat a fost creat cu success!");
+  response.redirect(`/campgrounds/${camp._id}`);
+};
+
+module.exports.showCampground = async (request, response) => {
+  const { id } = request.params;
+  const campground = await Campground.findById(id)
+    .populate({ path: "reviews", populate: { path: "author" } })
+    .populate("author");
+  if (!campground) {
+    request.flash("error", "Locul pe care doriți să-l accesați nu există!");
+    response.redirect("/campgrounds");
+  }
+  response.render("campgrounds/show", { campground });
+};
+
+module.exports.renderEditForm = async (request, response) => {
+  const { id } = request.params;
+  const campground = await Campground.findById(id);
+  if (!campground) {
+    request.flash("error", "Locul pe care doriți să-l editați nu există!");
+    response.redirect("/campgrounds");
+  }
+  response.render("campgrounds/edit", { campground });
+};
+
+module.exports.updateCampground = async (request, response) => {
+  const { id } = request.params;
+  const campground = await Campground.findByIdAndUpdate(id, {
+    ...request.body.campground,
+  });
+  request.flash("success", "Locul de campat a fost actualizat cu succes!");
+  response.redirect(`/campgrounds/${campground._id}`);
+};
+
+module.exports.deleteCampground = async (request, response) => {
+  try {
+    const { id } = request.params;
+    await Campground.findByIdAndDelete(id);
+    response.redirect("/campgrounds");
+  } catch (error) {
+    console.log(error);
+  }
+};
--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -1,97 +1,37 @@
 const express = require("express");
 const catchAsync = require("../utils/catchAsync");
-const Campground = require("../models/campground");
 const { isLoggedIn, isAuthor, validateCampground } = require("../middleware");
+const campgrounds = require("../controllers/campgrounds");
 
 const router = express.Router();
 
-router.get(
-  "/",
-  catchAsync(async (request, response) => {
-    const campgrounds = await Campground.find({});
-    response.render("campgrounds/index", { campgrounds });
-  })
-);
+router
+  .route("/")
+  .get(catchAsync(campgrounds.index))
+  .post(
+    isLoggedIn,
+    validateCampground,
+    catchAsync(campgrounds.createCampground)
+  );
 
-router.get(
-  "/new",
-  isLoggedIn,
-  catchAsync(async (request, response) => {
-    response.render("campgrounds/new");
-  })
-);
+router.get("/new", isLoggedIn, catchAsync(campgrounds.renderNewForm));
 
-router.post(
-  "/",
-  isLoggedIn,
-  validateCampground,
-  catchAsync(async (request, response, next) => {
-    const { campground } = request.body;
-    const camp = new Campground(campground);
-    camp.author = request.user._id;
-    await camp.save();
-    request.flash("success", "Locul de campat a fost creat cu success!");
-    response.redirect(`/campgrounds/${camp._id}`);
-  })
-);
-router.get(
-  "/:id",
-  catchAsync(async (request, response) => {
-    const { id } = request.params;
-    const campground = await Campground.findById(id)
-      .populate({ path: "reviews", populate: { path: "author" } })
-      .populate("author");
-    if (!campground) {
-      request.flash("error", "Locul pe care doriți să-l accesați nu există!");
-      response.redirect("/campgrounds");
-    }
-    response.render("campgrounds/show", { campground });
-  })
-);
+router
+  .route(":/id")
+  .get(catchAsync(campgrounds.showCampground))
+  .put(
+    validateCampground,
+    isLoggedIn,
+    isAuthor,
+    catchAsync(campgrounds.updateCampground)
+  )
+  .delete(isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground));
 
 router.get(
   "/:id/edit",
   isLoggedIn,
   isAuthor,
-  catchAsync(async (request, response) => {
-    const { id } = request.params;
-    const campground = await Campground.findById(id);
-    if (!campground) {
-      request.flash("error", "Locul pe care doriți să-l editați nu există!");
-      response.redirect("/campgrounds");
-    }
-    response.render("campgrounds/edit", { campground });
-  })
-);
-
-router.put(
-  "/:id",
-  validateCampground,
-  isLoggedIn,
-  isAuthor,
-  catchAsync(async (request, response) => {
-    const { id } = request.params;
-    const campground = await Campground.findByIdAndUpdate(id, {
-      ...request.body.campground,
-    });
-    request.flash("success", "Locul de campat a fost actualizat cu succes!");
-    response.redirect(`/campgrounds/${campground._id}`);
-  })
-);
-
-router.delete(
-  "/:id",
-  isLoggedIn,
-  isAuthor,
-  catchAsync(async (request, response) => {
-    try {
-      const { id } = request.params;
-      await Campground.findByIdAndDelete(id);
-      response.redirect("/campgrounds");
-    } catch (error) {
-      console.log(error);
-    }
-  })
+  catchAsync(campgrounds.renderEditForm)
 );
 
 module.exports = router;
